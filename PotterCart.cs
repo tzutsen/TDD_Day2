f6f486904c8cd4e530c63493c0bfe8046fc28b1c
tzutsen <eay926@gmail.com>
2017-06-20 18:21:40 +0800

    第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 (綠燈)

--- PotterCart.cs
+++ PotterCart.cs
@@ -5,17 +5,31 @@ namespace BooksStore.Models
 {
     public class PotterCart
     {
+        private Dictionary<int, int> _unitPrice = new Dictionary<int, int>();
+        private Dictionary<int, double> _discount = new Dictionary<int, double>();
+
         public PotterCart()
         {
+            _unitPrice.Add(1,100);
+            _discount.Add(1,1);
         }
 
-        public int CalculatePrice(Dictionary<int, int> books)
+        public PotterCart(Dictionary<int, int> price, Dictionary<int, double> discount)
         {
-            var totalPrice = 0;
+            _unitPrice = price;
+            _discount = discount;
+        }
+
+        public double CalculatePrice(Dictionary<int, int> books)
+        {
+            var booksCount = 0;
+            var priceBeforeDiscount = 0;
             foreach (var book in books)
             {
-                totalPrice += book.Value * 100;
+                priceBeforeDiscount += _unitPrice[book.Key];
+                booksCount++;
             }
+            double totalPrice = priceBeforeDiscount * _discount[booksCount];
             return totalPrice;
         }
     }
