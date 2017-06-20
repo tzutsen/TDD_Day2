6f94e6f56b3e9523f8a82a91e2e3f8607ddb40af
tzutsen <eay926@gmail.com>
2017-06-20 17:24:26 +0800

    第一集買了一本，其他都沒買，價格應為100*1=100元 (紅燈)

--- BooksStoreCartTest.cs
+++ BooksStoreCartTest.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BooksStore.Tests
 {
+    [TestClass]
     class BooksStoreCartTest
     {
+        private Dictionary<int, int> _books = new Dictionary<int, int>();
+
+        [TestMethod]
+        public void 第一集買了一本_其他都沒買_價格應為100()
+        {
+            // arrange
+            _books.Add(1,1);//key：第1集，value：1本
+
+            var target = new PotterCart();
+            // act
+            //程式計算的結果
+            int actual = target.CalculatePrice(_books);
+            //應該得到的結果
+            int expected = 100; 
+            
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
