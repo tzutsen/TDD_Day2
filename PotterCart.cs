394b7c5b1482559493f862e5ce334001e525f224
tzutsen <eay926@gmail.com>
2017-06-20 17:38:33 +0800

    第一集買了一本，其他都沒買，價格應為100*1=100元 (綠燈)

---
+++ PotterCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksStore.Models
+{
+    public class PotterCart
+    {
+        public PotterCart()
+        {
+        }
+
+        public int CalculatePrice(Dictionary<int, int> books)
+        {
+            var totalPrice = 0;
+            foreach (var book in books)
+            {
+                totalPrice += book.Value * 100;
+            }
+            return totalPrice;
+        }
+    }
+}
